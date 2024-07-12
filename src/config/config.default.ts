e9bcbc374a6327580ec0a5f6a9cd713b03d41241
yangxuan <yangxuan09@100.me>
2024-07-12 18:38:30 +0800

    feat: 增加静态资源服务

--- src/config/config.default.ts
+++ src/config/config.default.ts
@@ -4,6 +4,6 @@ export default {
   // use for cookie sign key, should change to your own and keep security
   keys: '1720767796435_3261',
   koa: {
-    port: 7001,
+    port: 7002,
   },
 } as MidwayConfig;
